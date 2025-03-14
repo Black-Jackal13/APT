798035d60db6e097a02489bfa548da6992ffe019
Black Jackal <louw.reyneke.otto@gmail.com>
2025-03-14 14:50:32 -0400

    Included App paths

--- APT/urls.py
+++ APT/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-# from django.contrib import admin
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+
+import APT_Django
 
 urlpatterns = [
-    #    path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", include(APT_Django.urls)),
 ]

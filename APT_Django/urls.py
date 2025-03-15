29fa9862a1964c4916c1a5734493b54c4d32047d
Black Jackal <louw.reyneke.otto@gmail.com>
2025-03-15 17:15:02 -0400

    Added paths to new pages

--- APT_Django/urls.py
+++ APT_Django/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("races", views.races, name="races"),
+    path("predictions", views.available_predictions, name="predict"),
+    path("results", views.current_results, name="results"),
 ]
--- APT_Django/views.py
+++ APT_Django/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
@@ -6,4 +5,19 @@ from django.template import loader
 # Create your views here.
 def index(request):
     page = loader.get_template("index.html")
+    return HttpResponse(page.render())
+
+
+def races(request):
+    page = loader.get_template("races.html")
+    return HttpResponse(page.render())
+
+
+def available_predictions(request):
+    page = loader.get_template("predictions.html")
+    return HttpResponse(page.render())
+
+
+def current_results(request):
+    page = loader.get_template("results.html")
     return HttpResponse(page.render())

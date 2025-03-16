f4f8a74ba602a2757f898fc9556fb0ba4f40b1e2
Black Jackal <louw.reyneke.otto@gmail.com>
2025-03-16 17:20:20 -0400

    Add initial models and migrations

--- APT_Django/models.py
+++ APT_Django/models.py
@@ -1,3 +1,73 @@
 from django.db import models
 
-# Create your models here.
+
+class RaceSeason(models.Model):
+    race_season_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.race_season_name
+
+
+class Race(models.Model):
+    race_season = models.ForeignKey(RaceSeason, on_delete=models.CASCADE)
+    race_name = models.CharField(max_length=100)
+    race_date = models.DateField()
+    race_finished = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.race_name
+
+
+class Players(models.Model):
+    player_name = models.CharField(max_length=100)
+    player_email = models.EmailField()
+    password = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.player_name
+
+
+class Predictions(models.Model):
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    player = models.ForeignKey(Players, on_delete=models.CASCADE)
+    player_prediction1 = models.TextField()
+    player_prediction2 = models.TextField()
+    player_prediction3 = models.TextField()
+
+    def __str__(self):
+        return self.player.player_name
+
+
+class RaceFinishers(models.Model):
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    place1 = models.TextField()
+    place2 = models.TextField()
+    place3 = models.TextField()
+    place4 = models.TextField()
+    place5 = models.TextField()
+    place6 = models.TextField()
+    place7 = models.TextField()
+    place8 = models.TextField()
+    place9 = models.TextField()
+    place10 = models.TextField()
+
+    def __str__(self):
+        return self.race.race_name
+
+
+class SeasonScores(models.Model):
+    season = models.ForeignKey(RaceSeason, on_delete=models.CASCADE)
+    player = models.ForeignKey(Players, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return self.player.player_name + " " + str(self.score)
+
+
+class RaceScores(models.Model):
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    player = models.ForeignKey(Players, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return self.player.player_name + " " + str(self.score)

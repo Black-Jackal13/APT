6506d6b19cf0d2cccbcf103baf4f2c38d801d703
Black Jackal <louw.reyneke.otto@gmail.com>
2025-03-16 17:43:47 -0400

    Register models in admin

--- APT_Django/admin.py
+++ APT_Django/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(RaceSeason)
+admin.site.register(Race)
+admin.site.register(Players)
+admin.site.register(Predictions)
+admin.site.register(RaceFinishers)
+admin.site.register(SeasonScores)
+admin.site.register(RaceScores)

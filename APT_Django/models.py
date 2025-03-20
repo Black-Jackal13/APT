1a423486ad4ec2b6ce1d7f34c3ab3c683d5b26c2
Black Jackal <louw.reyneke.otto@gmail.com>
2025-03-20 10:23:47 -0400

    Added field "ongoing" to RaceSeason model

--- APT_Django/models.py
+++ APT_Django/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class RaceSeason(models.Model):
     race_season_name = models.CharField(max_length=100)
+    ongoing = models.BooleanField(default=True)
 
     def __str__(self):
         return self.race_season_name

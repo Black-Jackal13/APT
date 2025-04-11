75e68d07f87776657ec25cc1ed5702bee687839f
Black Jackal <louw.reyneke.otto@gmail.com>
2025-04-11 08:16:34 -0400

    Created Registration Form for players

--- APT_Django/forms.py
+++ APT_Django/forms.py
@@ -6,3 +6,9 @@ class EnterPrediction(forms.Form):
     prediction_1 = forms.CharField(max_length=100)
     prediction_2 = forms.CharField(max_length=100)
     prediction_3 = forms.CharField(max_length=100)
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    email = forms.EmailField(max_length=100)
+    verify_email = forms.EmailField(max_length=100)
